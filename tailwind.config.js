753617f0a5c3b6b24ec7c6930fd23629063f5eec
SKIBIDIBOSS <aarav.nai66@gmail.com>
2026-05-30 08:22:55 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,迎え,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#09090b',
+        panel: '#18181b',
+        accent: '#6366f1'
+      }
+    },
+  },
+  plugins: [],
+}
